4ae457fef188a0ba67bf037a24da041b85a66312
Midlaj994 <mohammedmidlaj83@gmail.com>
2025-11-22 19:58:43 +0530

    Update urls.py

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from .views import patient_book, doctor_upcoming, admin_dashboard, admin_confirm, admin_cancel, doctor_notes
 
+from .views import (
+    patient_book,
+    doctor_upcoming,
+    doctor_notes,
+    admin_dashboard,
+    admin_confirm,
+    admin_cancel,
+)
 
 urlpatterns = [
     path("book/", patient_book, name="patient_book"),
-    path("doctor/upcoming/", doctor_upcoming, name="doctor_upcoming"),
-    path("doctor/appointments/<int:pk>/notes/", doctor_notes, name="doctor_notes"),
-    path("admin/dashboard/", admin_dashboard, name="admin_dashboard"),
+    path("doctor/", doctor_upcoming, name="doctor_upcoming"),
+    path("doctor/notes/<int:pk>/", doctor_notes, name="doctor_notes"),
+    path("admin/", admin_dashboard, name="admin_dashboard"),
     path("admin/confirm/<int:pk>/", admin_confirm, name="admin_confirm"),
     path("admin/cancel/<int:pk>/", admin_cancel, name="admin_cancel"),
 ]
